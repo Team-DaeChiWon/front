abe8d83dd1e47dcac77b8d8d01fa4907875f7c1f
오은지 <81677536+oheunji05@users.noreply.github.com>
2022-08-28 17:59:02 +0900

    [add]

--- src/Register.js
+++ src/Register.js
@@ -1,7 +1,14 @@
 import React, { useState } from "react"
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 function Register(){
+
+    let [id, setId] = useState();
+    let [password, setPassword] = useState();
+
+    let navigate = useNavigate();
+
     return(
         <div>
 
