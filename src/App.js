9814317fc6a313746645b4b8063e3cb3fffe9544
오은지 <81677536+oheunji05@users.noreply.github.com>
2022-08-28 17:57:35 +0900

    [add]

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react"
 import {Routes, Route, Link} from 'react-router-dom';
 
 import Login from './Login.js';
+import Register from './Register.js';
 
 function App(){
     return(
         <div>
             <Routes>
                 <Route path='/login' element={<Login></Login>}></Route>
+                <Route path='/register' element={<Register></Register>}></Route>
             </Routes>
         </div>
     )
--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,32 @@
 import React, { useState } from "react"
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
+let Box = styled.div`
+    width: 850px;
+    height: 525px;
+    border-radius: 10px;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 10%;
+    display: flex;
+    box-shadow: 3px 3px 3px 3px rgb(179, 179, 179);
+` 
+
+let SubBox_Left = styled.div`
+    display: flex;
+    width: 425px;
+    height: 525px;
+    background-color: #eee;
+    border-radius: 10px 0px 0px 10px;
+`
+
+let SubBox_Right = styled.div`
+    width: 425px;
+    height: 525px;
+    background-color: #eee;
+    border-radius: 0px 10px 10px 0px;
+`
 
 function Login(){
 
@@ -10,9 +36,10 @@ function Login(){
     let navigate = useNavigate();
 
     return(
-        <div>
-
-        </div>
+        <Box>
+            <SubBox_Left></SubBox_Left>
+            <SubBox_Right></SubBox_Right>
+        </Box>
     )
 }
 
