8957e198de0abefe947e91dd698f30597bcc6e96
오은지 <81677536+oheunji05@users.noreply.github.com>
2022-08-28 17:40:03 +0900

    [add]

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import {Routes, Route, Link} from 'react-router-dom';
+
+import Login from '.login.js';
+
+function App(){
+    return(
+        <div>
+            <Routes>
+                <Route path='/login' element={<Login></Login>}></Route>
+            </Routes>
+        </div>
+    )
+}
---
+++ src/Login.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react"
+import { useNavigate } from "react-router-dom";
+
+
+function Login(){
+
+    let [id, setId] = useState()
+    let [password, setPassword] = useState()
+
+    let navigate = useNavigate();
+
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default Login;
