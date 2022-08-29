01eed513f66b2798ea7fdcb1b5c56317fc958963
오은지 <81677536+oheunji05@users.noreply.github.com>
2022-08-29 12:26:25 +0900

    [add]

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,11 @@
 import React, { useState } from "react"
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import image from './image/login.png';
+import Main_image from './image/login.png';
+import or_img from './image/or_img.jpg';
+import axios from "axios";
+
+import './App.css';
 
 let Box = styled.div`
     width: 950px;
@@ -13,44 +17,107 @@ let Box = styled.div`
     display: flex;
 ` 
 
-let SubBox_Left = styled.div`
+let SubBox_Left = styled.div` //사진 들어갈 부분
     display: flex;
     width: 475px;
     height: 550px;
-    background-color: #eee;
+    //background-color: #eee;
     border-radius: 10px 0px 0px 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `
 
-let SubBox_Right = styled.div`
+let SubBox_Right = styled.div` //글 & 로그인 영역
     width: 475px;
     height: 550px;
-    background-color: #eee;
+    //background-color: #eee;
     border-radius: 0px 10px 10px 0px;
 `
 
-let Input = styled.input`
-    width: 280px;
-    height: 30px;
+let LoginBox = styled.div` //글 제외 로그인 영역
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+let Input = styled.input` //아이디, 비밀번호 input 영역
+    width: 300px;
+    height: 35px;
     border-radius: 10px;
     border: 0.5px solid;
     margin-bottom: 10px;
     //box-shadow: 1px 1px 0px 0px #8B8B8B;
+    outline: 0;
+    border: 1px solid black;
 `
 
-let Button = styled.button`
-    width: 287px;
-    height: 33px;
+let Button = styled.button` //로그인 버튼 영역
+    width: 307px;
+    height: 45px;
     border-radius: 10px;
     border: 0;
     outline: 0;
     background-color: #8B8B8B;
     //box-shadow: 1px 1px 0px 0px #8B8B8B;
-    margin-top: 20px;
+    margin-top: 30px;
 
+    color: white;
+    font-size: 20px;
+    font-weight: bold;
     &:hover{  
         background-color : white;
         color: black;
     }
+`
+
+let Keep = styled.div` //로그인 상태 유지 영역
+    display: flex;
+    margin-top: -20px;
+    margin-left: -165px;
+`
+
+let CheckBox = styled.input` //체크 박스 영역
+    width: 1.1rem;
+    height: 1.1rem;
+    border-radius: 50%;
+    border: 2px solid;
+    appearance: none;
+    cursor: pointer;
+`
+
+let LargeFont = styled.h1` //제일 큰 제목
+    font-size: 35px;
+    font-weight: bold;
+    margin-left: 90px;
+    margin-bottom: -5px;
+    margin-top: 50px;
+`
+
+let MiddleFont = styled.h3` //중간 사이즈 제목
+    font-size: 20px;
+    font-weight: bold;
+    color: #818181;
+`
+
+let Sub = styled.div` //회원가입 / 아이디찾기 / 비밀번호 찾기 페이지로 이동하는 구역
+    display: flex;
+    margin-top: 5px;
+`
+
+let Move_Register = styled.a` //회원가입 이동
+    font-size: 12px;
+    margin-left: 95px;
+`
+
+let Move_FindId = styled.a` //아이디 찾기 이동
+    font-size: 12px;
+    margin-left: 90px;
+`
+
+let Move_FindPassword = styled.a` //비밀번호 찾기 이동
+    font-size: 12px;
+    margin-left: 10px;
 `
 
 function Login(){
@@ -63,18 +130,45 @@ function Login(){
     return(
         <Box>
             <SubBox_Left>
-                <img src={image} width="400px" height="400px"/>
+                <img src={Main_image} width="500px" height="500px"/>
             </SubBox_Left>
             <SubBox_Right>
-                <h1>실버존</h1>
-                <h3>다른 서비스로 로그인</h3>
 
-                <Input placeholder="  아이디"></Input>
-                <Input placeholder="  비밀번호"></Input>
-                <br></br>
-                <input type="checkbox" name="keep"></input>
-                <div>로그인 상태 유지</div>
-                <Button>로그인</Button>
+                <LargeFont>실버존</LargeFont>
+
+                <LoginBox>
+
+                    <MiddleFont>다른 서비스로 로그인</MiddleFont>
+
+                    <img src={or_img} width="340px" height="30px"></img>
+
+                    <br/>
+
+                    <Input placeholder="  아이디" onChange={(e)=>{
+                        setId(e.target.value)
+                    }}></Input>
+                    <Input placeholder="  비밀번호" onChange={(e)=>{
+                        setPassword(e.target.value)
+                    }}></Input>
+
+                    <br></br>
+
+                    <Keep><CheckBox type="checkbox" name="keep"></CheckBox>로그인 상태 유지</Keep>
+
+                    <Button onClick={()=>{
+                        axios.post('',{asdf:id, asad:password})
+                        .then(()=>{})
+                    }}>로그인</Button>
+
+                </LoginBox>
+
+                <Sub>
+                    <Move_Register href="/register">회원가입</Move_Register>
+                    <Move_FindId href="/">아이디 찾기</Move_FindId>
+                    <Move_FindPassword href="/">비밀번호 찾기</Move_FindPassword>
+                    
+                </Sub>
+
             </SubBox_Right>
         </Box>
     )
