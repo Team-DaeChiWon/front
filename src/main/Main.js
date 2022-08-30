2584bfb916a913f0cb7352a727275762fad3f8b3
김민지 <wehouse201@naver.com>
2022-08-30 21:44:53 +0900

    main

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,11 +1,37 @@
 import React from "react"
 import Header from "../Header";
+import MainImg from "../image/main.svg"
+import MainImg1 from "../image/main1.svg"
+import MainImg2 from "../image/main2.svg"
+
+import {Carousel } from 'react-bootstrap';
 
 function Main(){
   return(
     <>
     <Header/>
-    
+    <Carousel fade>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={MainImg}
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={MainImg1}
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={MainImg2}
+        />
+      </Carousel.Item>
+    </Carousel>
+
+
     </>
   )
 }
