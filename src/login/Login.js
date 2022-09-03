23fa3320c89b9d56b19676ee5295f4f18a7b9438
오은지 <81677536+oheunji05@users.noreply.github.com>
2022-09-03 23:28:00 +0900

    [add]

--- src/login/Login.js
+++ src/login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Main_image from '../image/login.png';
 import or_img from '../image/or_img.jpg';
