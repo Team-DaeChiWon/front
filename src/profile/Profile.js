cd078733ea4014bcf9db0096b3ce54eb92621c4f
오은지 <81677536+oheunji05@users.noreply.github.com>
2022-09-05 22:16:51 +0900

    [update]

--- src/profile/Profile.js
+++ src/profile/Profile.js
@@ -2,12 +2,55 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+
+
 import * as P from './Profile.style.js';
 
 function Profile(){
+
+    let [nowPassword, setNowPassword] = useState();
+    let [newPassword, setNewPassword] = useState();
+    let [checkPassword, setCheckPassword] = useState();
+
     return(
+
         <P.BigBox>
-            
+
+            <P.Main>
+                <P.Img></P.Img>
+                <P.Name>이름</P.Name>
+            </P.Main>
+
+            <Tabs
+                defaultActiveKey="home"
+                transition={false}
+                id="noanim-tab-example"
+                className="mb-3"
+                >
+
+                <Tab eventKey="home" title="정보">
+                아
+                </Tab>
+
+                <Tab eventKey="profile" title="보안">
+                    <P.Change_Password_Part>
+                        <P.H5>현재 비밀번호</P.H5>
+                        <P.Input placeholder="현재 비밀번호"></P.Input>
+                        <P.H5>새 비밀번호</P.H5>
+                        <P.Input placeholder="새 비밀번호"></P.Input>
+                        <P.H5>비밀번호 확인</P.H5>
+                        <P.Input placeholder="비밀번호 확인"></P.Input>
+                    </P.Change_Password_Part>
+                </Tab>
+
+                <Tab eventKey="contact" title="글로그">
+                우
+                </Tab>
+
+            </Tabs>
+
         </P.BigBox>
     )
 }
--- src/profile/Profile.style.js
+++ src/profile/Profile.style.js
@@ -2,6 +2,50 @@ import styled from "styled-components";
 
 export const BigBox = styled.div`
     background-color: aliceblue;
-    width: 800px;
-    height: 500px;
+    width: 900px;
+    height: 600px;
+    margin-left: auto;
+    margin-right: auto;
+    border-radius: 10px;
+    margin-top: 160px;
+`
+
+export const Main = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`
+
+export const Name = styled.h1`
+    font-size: 18px;
+`
+
+export const Img = styled.div`
+    width: 150px;
+    height: 150px;
+    border-radius: 100%;
+    background-color: wheat;
+    margin: 20px;
+`
+
+export const Change_Password_Part = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`
+
+export const Input = styled.input`
+    width: 400px;
+    height: 35px;
+    margin-bottom: 10px;
+    margin-top: 10px;
+    border: 0;
+    outline: 0;
+    border-radius: 10px;
+`
+
+export const H5 = styled.h5`
+    margin-right: 300px;
 `
