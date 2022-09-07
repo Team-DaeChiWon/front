cebbe98236b27c3fb2523635358379908a589f5a
오은지 <81677536+oheunji05@users.noreply.github.com>
2022-09-07 23:50:34 +0900

    [update]

--- src/profile/Profile.js
+++ src/profile/Profile.js
@@ -8,12 +8,16 @@ import Tabs from 'react-bootstrap/Tabs';
 import profile from '../image/profile.png';
 
 import * as P from './Profile.style.js';
+import './Profile.css';
 
 function Profile(){
 
     let [nowPassword, setNowPassword] = useState();
     let [newPassword, setNewPassword] = useState();
     let [checkPassword, setCheckPassword] = useState();
+    let [name, setName] = useState();
+
+    let navigate = useNavigate();
 
     return(
         <div className="backgroundColor">
@@ -24,32 +28,45 @@ function Profile(){
                 <P.Name>이름</P.Name>
             </P.Main>
 
-            <Tabs
+            <Tabs 
                 defaultActiveKey="home"
                 transition={false}
                 id="noanim-tab-example"
-                className="mb-3"
+                className="mb-3; TabsFlex"
                 >
 
                 <Tab eventKey="home" title="정보">
-                아
+                    <P.Box>
+                    </P.Box>
                 </Tab>
 
                 <Tab eventKey="profile" title="보안">
-                    <P.Change_Password_Part>
+                    <P.Box>
                         <P.fontSize>현재 비밀번호</P.fontSize>
-                        <P.Input placeholder="현재 비밀번호" onChange={(e)=>{nowPassword(e.target.value)}}></P.Input>
+                        <P.Input placeholder="현재 비밀번호" onChange={(e)=>{setNowPassword(e.target.value)}}></P.Input>
 
                         <P.fontSize>새 비밀번호</P.fontSize>
-                        <P.Input placeholder="새 비밀번호" onChange={(e)=>{newPassword(e.target.value)}}></P.Input>
+                        <P.Input placeholder="새 비밀번호" onChange={(e)=>{setNewPassword(e.target.value)}}></P.Input>
 
                         <P.fontSize>비밀번호 확인</P.fontSize>
-                        <P.Input placeholder="비밀번호 확인" onChange={(e)=>{checkPassword(e.target.value)}}></P.Input>
-                    </P.Change_Password_Part>
+                        <P.Input placeholder="비밀번호 확인" onChange={(e)=>{setCheckPassword(e.target.value)}}></P.Input>
+
+                        {
+                            newPassword != "" && checkPassword != "" ? 
+                            newPassword === checkPassword ? <P.Same>비밀번호가 일치합니다</P.Same> : <P.Different>*비밀번호가 일치하지 않습니다*</P.Different>
+                            : null
+                        }
+
+                        <P.Button onClick={()=>{
+                            axios.post('',{asdf:nowPassword, asdf:newPassword})
+                            .then((result)=>{navigate('')})
+                        }}>변경하기</P.Button>
+                    </P.Box>
                 </Tab>
 
                 <Tab eventKey="contact" title="글로그">
-                우
+                    <P.Box>
+                    </P.Box>
                 </Tab>
 
             </Tabs>
