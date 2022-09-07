17b3b6f11c80bf781f63e99d2c45390539af6f4b
김민지 <wehouse201@naver.com>
2022-09-07 11:13:07 +0900

    user

--- src/main/item/Member.js
+++ src/main/item/Member.js
@@ -1,4 +1,5 @@
 import "../../style/main.css";
+import User from "../../image/user.svg";
 
 function Member() {
   return(
@@ -7,11 +8,16 @@ function Member() {
       <div className="menu">
         
       </div>
-      <div className="mbBox">
-        <div className="mbimg">
-          <h6>닉네임</h6>
-          <p className="mbp">분류</p>
+      <div className="mbBox"> 
+        <div className="mbimg user">
+          <img className="userimg" src={User}/>
+          <div className="userbox">
+            <h6 style={{fontWeight:'700'}}>닉네임</h6>
+            <p style={{color:'#828282'}}>분류</p>
+          </div>
         </div>
+
+        <div className="mb-menu user"></div>
       </div>
     </div>
     </>
