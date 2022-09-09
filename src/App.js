1bdd223e7c9121bc8045e18338a58c676ac612ae
Sswmhskr06ksj <81690443+Sswmhskr06ksj@users.noreply.github.com>
2022-09-09 22:01:01 +0900

    feat : EducationPlan

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from "react"
-import {Routes, Route, Link} from 'react-router-dom';
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
 
-import Login from './login/Login.js';
-import Register from './login/Register.js';
-import Header from './Header.js';
-import Main from './main/Main.js';
-import Profile from './profile/Profile.js';
-import Writing from './writing/Writing.js';
+import Login from "./login/Login.js";
+import Register from "./login/Register.js";
+import Header from "./Header.js";
+import Main from "./main/Main.js";
+import Profile from "./profile/Profile.js";
+import Writing from "./writing/Writing.js";
+import EducationPlan from "./educationplan/EducationPlan.jsx";
 
-function App(){
-    return(
-        <div>
-            <Routes>
-                <Route path='/login' element={<Login></Login>}></Route>
-                <Route path='/register' element={<Register></Register>}></Route>
-                <Route path='/header' element={<Header></Header>}></Route>
-                <Route path='/' element={<Main></Main>}></Route>
-                <Route path='/profile' element={<Profile></Profile>}></Route>
-                <Route path='/writing' element={<Writing/>}></Route>
-            </Routes>
-        </div>
-    )
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/login" element={<Login></Login>} />
+        <Route path="/register" element={<Register></Register>} />
+        <Route path="/header" element={<Header></Header>} />
+        <Route path="/" element={<Main></Main>} />
+        <Route path="/profile" element={<Profile></Profile>} />
+        <Route path="/writing" element={<Writing />} />
+        <Route path="/educationplan" element={<EducationPlan />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/educationplan/EducationPlan.jsx
+++ src/educationplan/EducationPlan.jsx
@@ -1,11 +1,126 @@
 import styled from "styled-components";
+import education1 from "../image/education1.png";
+import education2 from "../image/education2.png";
+import education3 from "../image/education3.png";
+import { useState, useEffect } from "react";
+import { AiOutlineLeft } from "react-icons/ai";
+import { AiOutlineRight } from "react-icons/ai";
+import * as E from "./EducationPlan.style";
 
-const Container = styled.div`
-  width: 500px;
-`;
+const data = [
+  [
+    {
+      image: education1,
+      title: "제목 2",
+    },
+    {
+      image: education3,
+      title: "제목 3",
+    },
+    {
+      image: education2,
+      title: "제목 1",
+    },
+    {
+      image: education2,
+      title: "제목 1  ",
+    },
+    {
+      image: education1,
+      title: "제목 3",
+    },
+    {
+      image: education3,
+      title: "제목 2",
+    },
+    {
+      image: education1,
+      title: "제목 1",
+    },
+  ],
+  [
+    {
+      image: education1,
+      title: "제목 2",
+    },
+    {
+      image: education3,
+      title: "제목 3",
+    },
+    {
+      image: education1,
+      title: "제목 2",
+    },
+    {
+      image: education3,
+      title: "제목 3",
+    },
+    {
+      image: education2,
+      title: "제목 1",
+    },
+  ],
+  [
+    {
+      image: education3,
+      title: "제목 3",
+    },
+  ],
+];
 
 const EducationPlan = () => {
-  return <></>;
+  const [period, setPeriod] = useState(0);
+  const [what, setWhat] = useState("월간");
+  const onClickLeftBtn = () => {
+    setPeriod(period - 1);
+  };
+
+  const onClickRightBtn = () => {
+    setPeriod(period + 1);
+  };
+  useEffect(() => {
+    period === 0
+      ? setWhat("월간")
+      : period === 1
+      ? setWhat("주간")
+      : setWhat("일간");
+  }, [period]);
+
+  return (
+    <E.Container>
+      <E.Title>
+        <h1>교육 계획안</h1>
+        <hr />
+      </E.Title>
+      <E.InfoContainer>
+        <E.Date>
+          {period !== 0 && (
+            <AiOutlineLeft
+              onClick={onClickLeftBtn}
+              style={{ marginTop: "11px" }}
+            />
+          )}
+          <p>{what}</p>
+          {period !== 2 && (
+            <AiOutlineRight
+              onClick={onClickRightBtn}
+              style={{ marginTop: "11px" }}
+            />
+          )}
+        </E.Date>
+        <E.Main>
+          {data[period].map((d, i) => {
+            return (
+              <E.Info key={i}>
+                <img src={d.image} />
+                <p>{d.title}</p>
+              </E.Info>
+            );
+          })}
+        </E.Main>
+      </E.InfoContainer>
+    </E.Container>
+  );
 };
 
 export default EducationPlan;
