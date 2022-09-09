d9f4b31dba4487417a61f0c278755408e87d17f9
진영 <81845425+baejin1018@users.noreply.github.com>
2022-09-09 23:39:10 +0900

    fix : Header

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,44 +1,33 @@
-import React from "react"
+import React from "react";
 import Header from "../Header";
-import MainImg from "../image/main.svg"
-import MainImg1 from "../image/main1.svg"
-import MainImg2 from "../image/main2.svg"
-import Footer from "../image/footer.svg"
+import MainImg from "../image/main.svg";
+import MainImg1 from "../image/main1.svg";
+import MainImg2 from "../image/main2.svg";
+import Footer from "../image/footer.svg";
 import Member from "./item/Member";
-import '../style/main.css'
+import "../style/main.css";
 
-import {Carousel} from 'react-bootstrap';
+import { Carousel } from "react-bootstrap";
 
-function Main(){
-  return(
+function Main() {
+  return (
     <>
-    <Header/>
-    <Carousel fade>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={MainImg}
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={MainImg1}
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={MainImg2}
-        />
-      </Carousel.Item>
-    </Carousel>
-    
-    <Member/>
-    <img  className='footer' src={Footer}/>
+      <Carousel fade>
+        <Carousel.Item>
+          <img className="d-block w-100" src={MainImg} />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={MainImg1} />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={MainImg2} />
+        </Carousel.Item>
+      </Carousel>
 
+      <Member />
+      <img className="footer" src={Footer} />
     </>
-  )
+  );
 }
 
 export default Main;
--- src/profile/Profile.js
+++ src/profile/Profile.js
@@ -2,78 +2,95 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
-import Tab from 'react-bootstrap/Tab';
-import Tabs from 'react-bootstrap/Tabs';
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
 
-import profile from '../image/profile.png';
+import profile from "../image/profile.png";
 
-import * as P from './Profile.style.js';
-import './Profile.css';
+import * as P from "./Profile.style.js";
+import "./Profile.css";
 
-function Profile(){
+function Profile() {
+  let [nowPassword, setNowPassword] = useState();
+  let [newPassword, setNewPassword] = useState();
+  let [checkPassword, setCheckPassword] = useState();
+  let [name, setName] = useState();
 
-    let [nowPassword, setNowPassword] = useState();
-    let [newPassword, setNewPassword] = useState();
-    let [checkPassword, setCheckPassword] = useState();
-    let [name, setName] = useState();
+  let navigate = useNavigate();
 
-    let navigate = useNavigate();
+  return (
+    <P.Container>
+      <P.BigBox>
+        <P.Main>
+          <P.Img src={profile}></P.Img>
+          <P.Name>이름</P.Name>
+        </P.Main>
 
-    return(
-        <div className="backgroundColor">
-        <P.BigBox>
+        <Tabs
+          defaultActiveKey="home"
+          transition={false}
+          id="noanim-tab-example"
+          className="mb-3; TabsFlex"
+        >
+          <Tab eventKey="home" title="정보">
+            <P.Box></P.Box>
+          </Tab>
 
-            <P.Main>
-                <P.Img src={profile}></P.Img>
-                <P.Name>이름</P.Name>
-            </P.Main>
+          <Tab eventKey="profile" title="보안">
+            <P.Box>
+              <P.fontSize>현재 비밀번호</P.fontSize>
+              <P.Input
+                placeholder="현재 비밀번호"
+                onChange={(e) => {
+                  setNowPassword(e.target.value);
+                }}
+              ></P.Input>
 
-            <Tabs 
-                defaultActiveKey="home"
-                transition={false}
-                id="noanim-tab-example"
-                className="mb-3; TabsFlex"
-                >
+              <P.fontSize>새 비밀번호</P.fontSize>
+              <P.Input
+                placeholder="새 비밀번호"
+                onChange={(e) => {
+                  setNewPassword(e.target.value);
+                }}
+              ></P.Input>
 
-                <Tab eventKey="home" title="정보">
-                    <P.Box>
-                    </P.Box>
-                </Tab>
+              <P.fontSize>비밀번호 확인</P.fontSize>
+              <P.Input
+                placeholder="비밀번호 확인"
+                onChange={(e) => {
+                  setCheckPassword(e.target.value);
+                }}
+              ></P.Input>
 
-                <Tab eventKey="profile" title="보안">
-                    <P.Box>
-                        <P.fontSize>현재 비밀번호</P.fontSize>
-                        <P.Input placeholder="현재 비밀번호" onChange={(e)=>{setNowPassword(e.target.value)}}></P.Input>
+              {newPassword != "" && checkPassword != "" ? (
+                newPassword === checkPassword ? (
+                  <P.Same>비밀번호가 일치합니다</P.Same>
+                ) : (
+                  <P.Different>*비밀번호가 일치하지 않습니다*</P.Different>
+                )
+              ) : null}
 
-                        <P.fontSize>새 비밀번호</P.fontSize>
-                        <P.Input placeholder="새 비밀번호" onChange={(e)=>{setNewPassword(e.target.value)}}></P.Input>
+              <P.Button
+                onClick={() => {
+                  axios
+                    .post("", { asdf: nowPassword, asdf: newPassword })
+                    .then((result) => {
+                      navigate("");
+                    });
+                }}
+              >
+                변경하기
+              </P.Button>
+            </P.Box>
+          </Tab>
 
-                        <P.fontSize>비밀번호 확인</P.fontSize>
-                        <P.Input placeholder="비밀번호 확인" onChange={(e)=>{setCheckPassword(e.target.value)}}></P.Input>
-
-                        {
-                            newPassword != "" && checkPassword != "" ? 
-                            newPassword === checkPassword ? <P.Same>비밀번호가 일치합니다</P.Same> : <P.Different>*비밀번호가 일치하지 않습니다*</P.Different>
-                            : null
-                        }
-
-                        <P.Button onClick={()=>{
-                            axios.post('',{asdf:nowPassword, asdf:newPassword})
-                            .then((result)=>{navigate('')})
-                        }}>변경하기</P.Button>
-                    </P.Box>
-                </Tab>
-
-                <Tab eventKey="contact" title="글로그">
-                    <P.Box>
-                    </P.Box>
-                </Tab>
-
-            </Tabs>
-
-        </P.BigBox>
-        </div>
-    )
+          <Tab eventKey="contact" title="글로그">
+            <P.Box></P.Box>
+          </Tab>
+        </Tabs>
+      </P.BigBox>
+    </P.Container>
+  );
 }
 
 export default Profile;
