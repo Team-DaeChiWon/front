051397ce1a4793f0036d1dc1e44bda265e7977cd
Hwanginyoung <hwang5375@gmail.com>
2022-09-10 17:36:18 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 
-<<<<<<< baejin1018
 import Login from "./login/Login.js";
 import Register from "./login/Register.js";
 import Header from "./Header.js";
 import Main from "./main/Main.js";
 import Profile from "./profile/Profile.js";
 import Writing from "./writing/Writing.js";
+import Share from "./share/Share.js";
+
 
 function App() {
   return (
